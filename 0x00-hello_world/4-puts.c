bf3966d408aff49d9432148d9af54d89910099bf
boatengjulius14 <boatengjulius14@gmail.com>
2022-12-12 21:00:32 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-  *puts would print out its function argument
-  */
+   * main - Beginning point
+    * Return: a 0 as success is predicted
+     */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
