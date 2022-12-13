70ec9c91eaf116dc9d4eedad632c6a94f7417f2f
boatengjulius14 <boatengjulius14@gmail.com>
2022-12-13 00:26:13 +0000

    Patience, persistence and perspiration

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - prints all possible combinations of single-digit numbers
+* Return: 0 for success
+*
+*/
+int main(void)
+
+{
+	int digit = '0';
+
+	while (digit <= '9')
+	{
+		putchar(digit);
+		if (digit != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+
+}
