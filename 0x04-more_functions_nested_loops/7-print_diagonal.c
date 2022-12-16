227061c584b606b987aa01ba6bbcbb4492d27123
boatengjulius14 <boatengjulius14@gmail.com>
2022-12-16 10:03:00 +0000

    I feel like a I am diagonally parked

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,7 +7,7 @@
 
 void print_diagonal(int n)
 {
-	int i, j;
+	int a, b;
 
 	if (n <= 0)
 	{
@@ -15,9 +15,9 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		for (i = 0; i < n; i++)
+		for (a = 0; a < n; a++)
 		{
-			for (j = 0; j < i; j++)
+			for (b = 0; b < a; b++)
 			{
 				_putchar(' ');
 			}
