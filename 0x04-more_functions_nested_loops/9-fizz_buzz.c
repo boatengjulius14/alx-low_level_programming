635cf2f6b0a46a213f73e3527eceafa6878b6b9b
boatengjulius14 <boatengjulius14@gmail.com>
2022-12-16 14:04:07 +0000

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ *main - print 1-100 and replace multiples of 3, 5 and both 3 and 5 with Fizz,
+ *Buzz, and FizzBuzz respectively
+ *Return: 0 always
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 != 0)
+		{
+		printf("Fizz ");
+		}
+		else if (a % 5 == 0 && a % 3 != 0)
+		{
+		printf("Buzz ");
+		}
+		else if (a % 5 == 0 && a % 3 == 0)
+		{
+		printf("FizzBuzz ");
+		}
+		else
+		{
+		printf("%d ", a);
+		}
+	}
+	printf("\n");
+	return (0);
+}
