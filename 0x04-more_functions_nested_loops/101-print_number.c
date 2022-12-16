57abfc3f50a99222aae399c03d3c2e4ff64f9bf6
boatengjulius14 <boatengjulius14@gmail.com>
2022-12-16 19:46:40 +0000

    Numbers have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,21 +1,22 @@
-#include "main.h"
+#include"main.h"
 
 /**
- *print_number - prints an integer
- *@n - integer
- */
+ * print_number - prints an integer
+ * @n: integer
+*/
 
 void print_number(int n)
 {
-	if (n / 10)
-	{
-		print_number(n / 10);
-	}
+	unsigned int i = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		i = -i;
 	}
-	_putchar(n % 10 + '0');
-}
 
+	if ((i / 10) > 0)
+		print_number(i / 10);
+
+	_putchar((i % 10) + 48);
+}
