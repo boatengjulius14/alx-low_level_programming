2801953e7fbb1816d1ab1af5529ef268d73e3df4
boatengjulius14 <boatengjulius14@gmail.com>
2022-12-16 19:39:35 +0000

    Numbers have life

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_number - prints an integer
+ *@n - integer
+ */
+
+void print_number(int n)
+{
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	_putchar(n % 10 + '0');
+}
+
