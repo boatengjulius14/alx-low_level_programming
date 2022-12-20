68509db4800f1fe9ef3e04d1ed10d6ef5be5cd9f
boatengjulius14 <boatengjulius14@gmail.com>
2022-12-20 13:08:28 +0000

    I do not fear computers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int sum;
+
+	while (s[sum] != '\0')
+	{
+		sum++;
+	}
+
+	while (i >= 0)
+	{
+		_putchar(s[sum]);
+		sum--;
+	}
+	_putchar('\n');
+}
