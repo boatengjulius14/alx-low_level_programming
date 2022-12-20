0c521129ecc6495186efa8589633a4aebcd17b46
boatengjulius14 <boatengjulius14@gmail.com>
2022-12-20 11:11:14 +0000

    This report, by its very length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the lenth of a string
+ *@s: pointer
+ *return: result of the string length
+ */
+
+int _strlen(char *s)
+{
+	int result;
+
+	result = strlen(s);
+	return (result);
+}
