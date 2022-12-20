40cdb78d24095dc1460321601e388829a395f556
boatengjulius14 <boatengjulius14@gmail.com>
2022-12-20 15:20:48 +0000

    A good engineer thinks

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *rev_string - reverses a string.
+ *@s: string
+ */
+
+void rev_string(char *s)
+{
+	int len = strlen(s);
+	int x = len - 1;
+
+	for (x; x >= 0; x--)
+	{
+		_putchar(s[x]);
+	}
+}
