94e9e94034a48034914130eb39e7c1f4f476e466
boatengjulius14 <boatengjulius14@gmail.com>
2023-01-03 15:09:50 +0000

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+include "main.h"
+
+/**
+ *_memcpy - copies memory area
+ *@dest: Address of destination array
+ *@src: Address of copied array
+ *@n: size of memory to be copied
+ *Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
