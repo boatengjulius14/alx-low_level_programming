e4b57b6b02282f785140cb054562712eff59db91
boatengjulius14 <boatengjulius14@gmail.com>
2023-01-03 16:31:01 +0000

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_strchr - locates a character in a string
+ *@s: address of string literal
+ *@c: character being searched for
+ *Return: s upon first location on c, 0 if c was not located
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (0);
+}
