6f23bf2b837db5eaa074cb90b11a4d50f3520ca5
boatengjulius14 <boatengjulius14@gmail.com>
2023-01-03 16:46:08 +0000

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,15 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (s[0] != '\0')
 	{
-		if (*s == c)
-		{
+		if (s[0] == c)
 			return (s);
-		}
 		s++;
+	}
+	if (s[0] == c)
+	{
+		return (s);
 	}
 	return (0);
 }
