e060cfe441126a0fc002bb39e9390372007aefbd
boatengjulius14 <boatengjulius14@gmail.com>
2023-01-04 14:01:58 +0000

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *_strstr - function that locates a substring
+ *@haystack: pointer to string 1
+ *@needle: pointer to string 2
+ *Return: oldhay, matching substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *oldhay = haystack;
+	char *oldnee = needle;
+
+	while (*haystack)
+	{
+		needle = oldnee;
+		haystack = oldhay;
+
+		while (*needle)
+		{
+			if (*haystack == *needle)
+			{
+				haystack++;
+				needle++;
+			}
+			else
+				break;
+		}
+		if (!*needle)
+			return (oldhay);
+		oldhay++;
+	}
+}
