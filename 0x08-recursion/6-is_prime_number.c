1746e30164907e2ee0ba1d3f755e2c9975a17bbf
boatengjulius14 <boatengjulius14@gmail.com>
2023-01-06 14:23:20 +0000

    Inception. Is it possible?

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *prime - for numbers > 2, it detects a prime number
+ *@n: number being analyzed
+ *@b: n - 1
+ *Return: 1 if n is a prime number, 0 if otherwise
+ */
+
+int prime(int n, int b)
+{
+	if (b < 2)
+		return (1);
+	if (n % b == 0 && b > 1)
+		return (0);
+	prime(n, b - 1);
+}
+
+/**
+ *is_prime_number - detects prime numbers
+ *@n: number being analyzed
+ *Return: 0, if number is less than 2, 1 if number is 2
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	if (n == 2)
+		return (1);
+	prime(n, n - 1);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,3 +4,5 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int _sqrt_recursion(int n);
 int sqroot(int n, int b);
+int is_prime_number(int n);
+int prime(int n, int b);
