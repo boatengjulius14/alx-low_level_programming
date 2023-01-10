a386bade0363beafd670de682e5d61249b7ba800
boatengjulius14 <boatengjulius14@gmail.com>
2023-01-10 18:41:49 +0000

    The woman who has no imagination

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_strdup - returns a pointer to a newly allocated space in memory with a
+ * copy of str
+ * @str: string to copy
+ * Return: NULL if str = NULL or space not allocated. arr for success
+ */
+
+char *_strdup(char *str)
+{
+	char *arr;
+	int i, count;
+
+	if (str == NULL)
+		return (NULL);
+
+	count = 0;
+	while (*(str + count))
+		count++;
+	arr = malloc(count * sizeof(char));
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < count; i++)
+		arr[i] = str[i];
+
+	return (arr);
+}
