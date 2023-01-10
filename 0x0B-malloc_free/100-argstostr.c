e71da94e7af0d62a1ac851b790c366035fbeb807
boatengjulius14 <boatengjulius14@gmail.com>
2023-01-10 21:23:36 +0000

    It isn't the mountains

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *argstostr - concatenates all the arugments of your program.
+ * @ac: argument count
+ * @av: array of input strings
+ * Return: arr, final string on success. NULL for error
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *arr;
+	int i, j, a, count = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			count++;
+			j++;
+		}
+		/*count++;*/
+		i++;
+	}
+	arr = malloc((sizeof(char) * count) + 1);
+	if (arr == NULL)
+		return (NULL);
+	i = 0;
+	for (i = 0; i < ac; i++)
+	{
+		a = 0;
+		j = 0;
+		while (av[i][j])
+		{
+			arr[a] = av[i][j];
+			a++;
+			j++;
+		}
+		arr[a] = '\n';
+		a++;
+	}
+	arr[a] = '\0';
+	return (arr);
