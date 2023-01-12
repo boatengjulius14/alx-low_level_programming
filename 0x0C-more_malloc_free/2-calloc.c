63b84eea0b9b9eee0921b9d7355a416de7c839c2
boatengjulius14 <boatengjulius14@gmail.com>
2023-01-12 15:13:40 +0000

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	char *ptr;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
@@ -26,5 +26,5 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	for (i = 0 ; i < nmemb ; i++)
 		ptr[i] = 0;
 
-	return (ptr);
+	return ((void *)ptr);
 }
