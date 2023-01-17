560c52d2b028aad938f61d25b0219834ed9464a3
boatengjulius14 <boatengjulius14@gmail.com>
2023-01-17 16:23:11 +0000

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef BLOCK
+#define BLOCK
+/**
+ * struct dog - defines a type struct dog
+ * @name: name
+ * @owner: owner
+ * @age: age
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+typedef struct dog doggo;
+#endif
