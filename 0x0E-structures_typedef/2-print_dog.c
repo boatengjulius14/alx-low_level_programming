166ead7d5df25795bd4c9faceb15e43099fd2549
boatengjulius14 <boatengjulius14@gmail.com>
2023-01-17 18:33:43 +0000

    A dog will teach you

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,24 +4,23 @@
 
 /**
  * print_dog - prints a struct dog
- * @d: pointer of structure to be printed
- * Return: nothing
+ * @d: pointer to structure to be printed
  */
 
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-	if (d->name != NULL)
-		printf("Name: %s\n", d->name);
-	else
-		printf("(nil)\n");
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
 
-	printf("Age: %f\n", d->age);
+		printf("Age: %f\n", d->age);
 
-	if (d->owner != NULL)
-		printf("Owner: %s\n", d->owner);
-	else
-		printf("(nil)\n");
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
 	}
 }
