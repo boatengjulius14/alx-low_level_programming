a6621eb9308c9aa155dd5577d08efedaba00811f
boatengjulius14 <boatengjulius14@gmail.com>
2023-01-17 19:55:21 +0000

    A door is what a dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _strlen - checks the length of a string
+ * @str: string to be analyzed
+ * Return: length of string
+ */
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len])
+		len++;
+	return (len);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: name
+ * @age: age
+ * @owner: owner name
+ * Return: pointer to new struct, or NULL
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int name_len, owner_len, i;
+	dog_t *dog;
+
+	dog = (dog_t *)malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	name_len = _strlen(name);
+	owner_len = _strlen(owner);
+
+	dog->name = malloc(sizeof(dog->name) * name_len);
+	if (dog->name == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < name_len; i++)
+		dog->name[i] = name[i];
+
+	dog->owner = malloc(sizeof(dog->owner) * owner_len);
+	if (dog->owner == NULL)
+	{
+		free(dog->owner);
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < owner_len; i++)
+		dog->owner[i] = owner[i];
+
+	dog->age = age;
+	return (dog);
+}
