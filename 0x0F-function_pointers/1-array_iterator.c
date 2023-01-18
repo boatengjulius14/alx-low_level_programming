f0be92b8cafdfa1f98e97c30f2d8f72ee28b548c
boatengjulius14 <boatengjulius14@gmail.com>
2023-01-18 17:57:46 +0000

    If you spend too much time

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (!action || !size)
+	if (!action || !size || !array)
 		return;
 	for (i = 0; i < size; i++)
 		action(array[i]);
