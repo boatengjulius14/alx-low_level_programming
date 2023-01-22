d06615c31f080907d3ad952e7273df090ae10440
boatengjulius14 <boatengjulius14@gmail.com>
2023-01-22 13:26:33 +0000

    To be is a to be the value

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,47 +5,43 @@
  * print_all - prints anything
  * @format: pointer to list of types of arguments passed to the function
  */
-
 void print_all(const char * const format, ...)
 {
-	int i, j, _switch = 0;
-	char *nstr, *init = "cifs";
+	int i, _switch;
+	char *str;
 	va_list op;
 
 	va_start(op, format);
 	i = 0;
-	while (format[i] && format)
+	while (format && format[i])
 	{
-		j = 0;
-		while (init[j])
-		{
-			if (format[i] == init[j] && _switch)
-			{
-				printf(", ");
-				break;
-			} j++;
-		}
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(op, int)), _switch = 1;
+				printf("%c", va_arg(op, int));
+				_switch = 0;
 				break;
 			case 'i':
-				printf("%d", va_arg(op, int)), _switch = 1;
+				printf("%i", va_arg(op, int));
+				_switch = 0;
 				break;
 			case 'f':
-				printf("%f", va_arg(op, double)), _switch = 1;
+				printf("%f", va_arg(op, double));
+				_switch = 0;
 				break;
 			case 's':
-				nstr = va_arg(op, char*), _switch = 1;
-				if (nstr)
-				{
-					printf("%s", nstr);
-					break;
-				}
-				printf ("(nil)");
+				str = va_arg(op, char*);
+				if (!str)
+					str = "(nil)";
+				printf("%s", str);
+				_switch = 0;
+				break;
+			default:
+				_switch = 1;
 				break;
 		}
+		if (format[i + 1] && !_switch)
+			printf(", ");
 		i++;
 	}
 	printf("\n");
