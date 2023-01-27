1d42f122f526a558ca1eb468f7a4f1538bc24128
boatengjulius14 <boatengjulius14@gmail.com>
2023-01-27 19:08:17 +0000

    Add note

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * _print - prints before the main function is executed
+ */
+void _print(void) __attribute__ ((constructor));
+void _print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
