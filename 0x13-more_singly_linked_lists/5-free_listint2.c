95c56d419af7a2f7239d62e4ef23274bcbf0bbe0
boatengjulius14 <boatengjulius14@gmail.com>
2023-01-30 18:52:27 +0000

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,13 +7,11 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp = *head;
+	listint_t *tmp;
 
-	if (!head)
-		return;
 	while (*head)
 	{
-		tmp = tmp->next;
+		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
 	}
