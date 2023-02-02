7268de2cac51c03b27397a93b6d638be8e0bff91
boatengjulius14 <boatengjulius14@gmail.com>
2023-02-02 19:43:12 +0000

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,17 +7,11 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long long int weight = 1, saveweight = 0;
-	unsigned long int i;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	while (n)
-	{
-		for (i = 0; (1 << (i + 1)) <= n; i++)
-			weight = weight * 10;
-
-		n = n - (1 << i);
-		saveweight = saveweight + weight;
-		weight = 1;
-	}
-	printf("%lld", saveweight);
+	if (n & 1)
+		printf("1");
+	else
+		printf("0");
 }
